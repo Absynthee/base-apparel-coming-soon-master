b03c60a1ea4cf76b063dbadb1f8fa70ee42be0b5
Austin Spillman <151957257+Absynthee@users.noreply.github.com>
2024-06-22 18:23:04 +0100

    final commit

--- styles/scripts.js
+++ styles/scripts.js
@@ -1,1 +1,11 @@
-//
+document.getElementById("submitBtn").addEventListener("click", function(event){
+    var x = document.getElementById("email").value;
+    var regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    if (!regex.test(x)) {
+        var errors = document.getElementsByClassName("error");
+        for(var i = 0; i < errors.length; i++) {
+            errors[i].style.display = "block";
+        }
+        event.preventDefault();
+    }
+});
